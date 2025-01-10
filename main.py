4667a8ee5208fb9b33149eec7dcf2f234372a23f
zNitche <53448802+zNitche@users.noreply.github.com>
2025-01-10 17:07:24 +0100

    relay test setup

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from vsr import Server
+import threading
 
 
 def main():
     server = Server(port=8989)
+    replay_server = Server(port=9000, broadcaster=server, broadcast=True)
 
     try:
+        rst = threading.Thread(target=replay_server.run)
+
+        rst.start()
         server.run()
     except KeyboardInterrupt:
         server.stop()
